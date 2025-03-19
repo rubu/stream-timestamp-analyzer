2abfd5800616f088f0ba314cc7aa06fab28619b9
Rūdolfs Bundulis <rudolfs.bundulis@gmail.com>
2025-03-19 12:43:31 +0200

    can debug

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import logging
+import sys
 import time
 from typing import List, Dict
 from stream_analyzer import create_analyzer, StreamAnalyzer
@@ -7,14 +8,16 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 class StreamManager:
-    def __init__(self):
+    def __init__(self, debug: bool = False):
         self.analyzers: Dict[str, StreamAnalyzer] = {}
         self.queues = {}
+        self.debug = debug
 
     def add_stream(self, url: str):
         """Add a new stream to analyze"""
         try:
             analyzer = create_analyzer(url)
+            analyzer.debug = self.debug  # Enable debugging if manager is in debug mode
             queue = analyzer.start()
             self.analyzers[url] = analyzer
             self.queues[url] = queue
@@ -50,14 +53,15 @@ class StreamManager:
             self.remove_stream(url)
 
 def main():
-    # Example usage
-    manager = StreamManager()
+    # Check if debug mode is enabled
+    debug_mode = '--debug' in sys.argv
+    if debug_mode:
+        sys.argv.remove('--debug')
+    
+    manager = StreamManager(debug=debug_mode)
     
     # Add your stream URLs here
-    streams = [
-        "rtmp://example.com/live/stream1",
-        "http://example.com/stream/playlist.m3u8"
-    ]
+    streams = sys.argv[1:]  # Take all command line arguments after the script name
     
     try:
         # Add all streams
